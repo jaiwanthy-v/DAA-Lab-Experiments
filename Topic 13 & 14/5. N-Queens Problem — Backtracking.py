0c766e83414ddabea68833ee3b70cca87e087bd5
JAIWANTHY V <192565034.simats@saveetha.com>
2026-09-04 20:01:22 +0530

    Create 5. N-Queens Problem — Backtracking.py

---
+++ Topic 13 & 14/5. N-Queens Problem — Backtracking.py
@@ -0,0 +1,46 @@
+def is_safe(board, row, col, n):
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    i, j = row - 1, col - 1
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+
+    i, j = row - 1, col + 1
+    while i >= 0 and j < n:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+
+    return True
+
+
+def solve(board, row, n):
+    if row == n:
+        return True
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
+
+            if solve(board, row + 1, n):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+n = 4
+board = [[0] * n for _ in range(n)]
+
+solve(board, 0, n)
+
+print("One Valid Solution:")
+for row in board:
+    print(*row)
