6d374aac301bb80e5b289436fab9b553897d3b74
JAIWANTHY V <192565034.simats@saveetha.com>
2026-09-04 19:42:30 +0530

    Update 4. Subset Sum Problem.py

--- Topic 10/4. Subset Sum Problem.py
+++ Topic 10/4. Subset Sum Problem.py
@@ -1,1 +1,21 @@
+def subset_sum(arr, target):
+    n = len(arr)
+    dp = [[False] * (target + 1) for _ in range(n + 1)]
 
+    for i in range(n + 1):
+        dp[i][0] = True
+
+    for i in range(1, n + 1):
+        for j in range(1, target + 1):
+            if arr[i - 1] <= j:
+                dp[i][j] = (
+                    dp[i - 1][j] or
+                    dp[i - 1][j - arr[i - 1]]
+                )
+            else:
+                dp[i][j] = dp[i - 1][j]
+
+    return dp[n][target]
+
+
+print(subset_sum([3, 34, 4, 12, 5, 2], 9))
