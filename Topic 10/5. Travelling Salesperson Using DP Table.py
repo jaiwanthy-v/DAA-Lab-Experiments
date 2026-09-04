0b79157b7f6554661c998cc98b6ca7a7e1e11fb0
JAIWANTHY V <192565034.simats@saveetha.com>
2026-09-04 19:43:29 +0530

    Create 5. Travelling Salesperson Using DP Table.py

---
+++ Topic 10/5. Travelling Salesperson Using DP Table.py
@@ -0,0 +1,35 @@
+def tsp_dp(graph):
+    n = len(graph)
+    dp = [[float('inf')] * n for _ in range(1 << n)]
+
+    dp[1][0] = 0
+
+    for mask in range(1 << n):
+        for u in range(n):
+            if mask & (1 << u):
+                for v in range(n):
+                    if not mask & (1 << v):
+                        dp[mask | (1 << v)][v] = min(
+                            dp[mask | (1 << v)][v],
+                            dp[mask][u] + graph[u][v]
+                        )
+
+    ans = float('inf')
+
+    for i in range(1, n):
+        ans = min(
+            ans,
+            dp[(1 << n) - 1][i] + graph[i][0]
+        )
+
+    return ans
+
+
+graph = [
+    [0, 10, 15, 20],
+    [10, 0, 35, 25],
+    [15, 35, 0, 30],
+    [20, 25, 30, 0]
+]
+
+print("Minimum Cost =", tsp_dp(graph))
